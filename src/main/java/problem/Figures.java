18c002f98adf6261ef83fc1e863b8fdb0df7e7e1
Anvar228 <maminimil@yandex.ru>
2021-03-05 15:11:25 +0300

    kjhb

--- src/main/java/problem/Figures.java
+++ src/main/java/problem/Figures.java
@@ -100,6 +100,6 @@ public class Figures {
             gl.glEnd();
     }
 
-    }}
+    }}//
 
 
---
+++ src/main/java/problem/Rectangle.java
@@ -0,0 +1,15 @@
+package problem;
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GL2GL3;
+import java.util.Vector;
+
+public class Rectangle {
+    Vector2 pos1;
+    Vector2 pos2;
+    public  void render(){
+
+    }
+    }
+
+
